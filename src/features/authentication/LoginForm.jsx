cf0df987464af64895edc5978c22bdbab35bd169
mesutkacmaz <mesut.kacmaz@outlook.com>
2024-08-17 03:29:30 +0300

    add default values for direct login

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import SpinnerMini from "../../ui/SpinnerMini";
 import { useLogin } from "./useLogin.js";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("jane@example.com");
+  const [password, setPassword] = useState("12345678");
   const { login, isPending } = useLogin();
 
   function handleSubmit(e) {
